51d6cf558287e93d25bacd8629b278ded6f4c857
AlexP <san@ligusan.us>
2020-12-17 17:10:38 -0500

    some groovyness

--- src/main/groovy/us/ligusan/advent2020/d17/d17p2.groovy
+++ src/main/groovy/us/ligusan/advent2020/d17/d17p2.groovy
@@ -1,11 +1,10 @@
 package us.ligusan.advent2020.d17;
 
-def nextActive = [] as Set
-(new File(getClass().getResource('input.txt').toURI()).collect { it }).withIndex().each { line, lineIndex ->
-    Arrays.asList(line.toCharArray()).withIndex().each { activeChar, charIndex ->
-        if(activeChar == '#') nextActive << [charIndex, lineIndex, 0, 0]
+def nextActive = ((new File(getClass().getResource('input.txt').toURI()).collect { it }).withIndex().collectMany { line, lineIndex ->
+    Arrays.asList(line.toCharArray()).withIndex().findResults { activeChar, charIndex ->
+        activeChar == '#' ? [charIndex, lineIndex, 0, 0] : null
     }
-}
+}) as Set
 
 def ball = [-1..1, -1..1, -1..1, -1..1].combinations()
 
@@ -13,24 +12,22 @@ def ball = [-1..1, -1..1, -1..1, -1..1].combinations()
     println "${nextActive.size()} ${nextActive}"
 
     def active = nextActive
-    nextActive = [] as Set
 
     def processed = [] as Set
-    active.each { element ->
-        ball.each { i ->
+    nextActive = (active.collectMany { element ->
+        ball.findResults { i ->
             def check = [element, i].transpose()*.sum()
             if(processed.add(check)) {
-                int nnum = 0
-                ball.combinations().each { j ->
-                    if(!(j.every { it == 0 }) && active.contains([check, j].transpose()*.sum())) nnum++
+                int nnum = ball.sum { j ->
+                    !(j.every { it == 0 }) && active.contains([check, j].transpose()*.sum()) ? 1 : 0
                 }
 
                 //                println "0: ${check} ${active.contains(check)} ${nnum}"
 
-                if(nnum == 3 || nnum == 2 && active.contains(check)) nextActive << check
-            }
+                if(nnum == 3 || nnum == 2 && active.contains(check)) return check
+            } else return null
         }
-    }
+    }) as Set
 
     println "${it} ${nextActive.size()}"
 }
