8a1d88b1c4dd50284a61adc1c943f4be042f1e08
AlexP <san@ligusan.us>
2021-12-03 11:47:18 -0500

    day 3

--- advent-2021/src/main/groovy/us/ligusan/advent2021/d3/d3p1.groovy
+++ advent-2021/src/main/groovy/us/ligusan/advent2021/d3/d3p1.groovy
@@ -1,5 +1,13 @@
 package us.ligusan.advent2021.d3;
 
-//List<String> list = new File(getClass().getResource('example.txt').toURI()).collect {it}
+List<String> list = new File(getClass().getResource('input.txt').toURI()).collect {it}
 
-println "test"
+int size = list.size()
+
+def gama0 = list.collect {element -> element.collect {Integer.valueOf(it)} }.transpose()*.sum().collect { (it / size).round().toString() }
+int gama = Integer.parseInt(gama0.join(), 2)
+
+int epsilon = (2.power(gama0.size()) - 1) - gama
+
+println "gama=${gama}, epsilon=${epsilon}"
+println gama*epsilon
---
+++ advent-2021/src/main/groovy/us/ligusan/advent2021/d3/d3p2.groovy
@@ -0,0 +1,21 @@
+package us.ligusan.advent2021.d3;
+
+List<String> list = new File(getClass().getResource('input.txt').toURI()).collect {it}
+
+int size = list.size()
+
+def og = list
+def co = list
+for(int i = 0; og.size() > 1; i++) {
+	def ogMost = (og.collect {Integer.valueOf(it[i])}.sum() / og.size()).round().toString()
+	og = og.findAll { it[i] == ogMost }
+
+	if(co.size() > 1) {
+		def coMost = (co.collect {Integer.valueOf(it[i])}.sum() / co.size()).round().toString()
+		co = co.findAll { it[i] != coMost }
+	}
+}
+println "og=${og}"
+println "co=${co}"
+
+println Integer.parseInt(og.join(), 2) * Integer.parseInt(co.join(), 2)
