52280d4ed7c381768d21a6198eb6df46c1ca2e1b
san <san@SANG7>
2021-12-11 12:52:20 -0500

    day 11

---
+++ advent-2021/src/main/groovy/us/ligusan/advent2021/d11/d11p1.groovy
@@ -0,0 +1,55 @@
+package us.ligusan.advent2021.d11
+
+List<String> list = new File(getClass().getResource('input.txt').toURI()).collect {it}
+
+int ySize = list.size()
+int xSize = list[0].size()
+
+def d = list.collect { line ->
+    line.toCharArray().toList().collect() { Integer.valueOf(it.toString()) }
+}
+println d
+
+def cube = ((1..2).collect { -1..1 }).combinations()
+println cube
+
+int counter = 0
+(1..100).each {
+//    println "--- ${it}"
+    
+    d = d.collect { it.collect { it + 1 } }
+//    println "d=${d}"
+    
+    def curF = [] as Set
+    def newF = d.withIndex().collectMany { line, y ->
+        line.withIndex().findResults { e, x ->
+            e > 9 ? [x,y] : null
+        }
+    }
+    while(!newF.empty) {
+//        println "curF=${curF}"
+//        println "newF=${newF}"
+        
+        curF += newF
+
+        newF = newF.collectMany { xy ->
+            cube.findResults {
+                int nX = xy[0] + it[0]
+                int nY = xy[1] + it[1]
+
+                return nX >= 0 && nX < xSize && nY >= 0 && nY < ySize ? [nX, nY] : null
+            }
+        }
+        newF.removeAll(curF)
+//        println "newF=${newF}"
+        newF.each { d[it[1]][it[0]]++ }
+        newF = (newF.findAll { d[it[1]][it[0]] > 9 }).toSet()
+    }
+    
+    counter += curF.size()
+//    println "counter=${counter}"
+    
+    d = d.collect { it.collect { it > 9 ? 0 : it } }
+//    println "d=${d}"
+}
+println counter
