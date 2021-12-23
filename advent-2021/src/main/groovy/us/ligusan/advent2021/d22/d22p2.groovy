c40cf731699aafe4d3e2339857340c4ae0407dda
san <san@SANG7>
2021-12-22 23:03:20 -0500

    day 22

--- advent-2021/src/main/groovy/us/ligusan/advent2021/d22/d22p2.groovy
+++ advent-2021/src/main/groovy/us/ligusan/advent2021/d22/d22p2.groovy
@@ -1,6 +1,6 @@
 package us.ligusan.advent2021.d22
 
-def list = new File(getClass().getResource('example.txt').toURI()).collect { it }
+def list = new File(getClass().getResource('input.txt').toURI()).collect { it }
 
 def areas = []
 list.each { line ->
@@ -12,51 +12,63 @@ list.each { line ->
 areas.each { println it }
 println '---------------'
 
+def area(r) {
+    return (r.collate(2).collect { Math.abs(it[1] - it[0] + 1) }).inject(BigInteger.ONE) { m, n -> m*n }
+}
+
 def intersect(l, r) {
-	def ret = null
-	
-	int x0 = Math.max(l[1][0], r[1][0])
-	int x1 = Math.min(l[1][1], r[1][1])
-	if(x0 <= x1) {
-		int y0 = Math.max(l[1][2], r[1][2])
-		int y1 = Math.min(l[1][3], r[1][3])
-		if(y0 <= y1) {
-			int z0 = Math.max(l[1][4], r[1][4])
-			int z1 = Math.min(l[1][5], r[1][5])
-			if(z0 <= z1) {
-				ret = [x0, x1, y0, y1, z0, z1]
-			}
-		}
-	}
-	println "l=${l}, r=${r}, ret=${ret}"
-	
-	return ret
+    def ret = null
+    
+    int x0 = Math.max(l[0], r[0])
+    int x1 = Math.min(l[1], r[1])
+    if(x0 <= x1) {
+        int y0 = Math.max(l[2], r[2])
+        int y1 = Math.min(l[3], r[3])
+        if(y0 <= y1) {
+            int z0 = Math.max(l[4], r[4])
+            int z1 = Math.min(l[5], r[5])
+            if(z0 <= z1) {
+                ret = [x0, x1, y0, y1, z0, z1]
+            }
+        }
+    }
+//    println "l=${l}, r=${r}, ret=${ret}"
+    
+    return ret
 }
 
-def count(list, onof) {
-	println "list=${list}, onof=${onof}"
-	if(list.size() == 1) return 0
-	
-	
-
-	def subList = list.collect { it }
-	subList.removeLast()
-	
-	def ret = count(subList, onof)
-	def inter = subList.collect {
-		println "it=${it}"
-		(it[0] == list[-1][0] ? -1 : 1) * count(intersect(it, list[-1]))
-	}
-	println "inter=${inter}"
-	ret += inter.sum()
-	if(list[-1][0] == onof) {
-		def ll = ((list[-1][1].collate(2).collect { Math.abs(it[1] - it[0] + 1) }).inject(1) { m, n -> m*n })		
-		println "list[-1]=${list[-1]}, ll=${ll}"
-		ret += ll
-	}
-	
-	println "list=${list}, onof=${onof}, ret=${ret}"
-	return ret 
+def counter = BigInteger.ZERO
+def sMap = [:]
+areas.reverseEach { a ->
+//    println '---------'
+//    sMap.each { key, value ->
+//        println "key=${key}, value=${value}"
+//    }
+//    println '---------'
+    
+    (sMap.keySet().sort().reverse()).each { key ->
+        def value = sMap[key]
+//        println "key=${key}, value=${value}"
+        
+        def newI = value.findResults { intersect(it, a[1]) }
+//        println "newI=${newI}"
+        if(!newI.empty) {
+            def level = sMap[key+1]
+            if(level == null) sMap[key+1] = (level = [])
+            level.addAll(newI)
+            if(a[0]) {
+                def s = (newI.collect { area(it) }).sum()
+                counter += (key%2 == 1 ? -1 : 1) * s
+//                println "key=${key}, s=${s}, counter=${counter}"
+            }
+        }
+    }
+    def level = sMap[1]
+    if(level == null) sMap[1] = (level = [])
+    level << a[1]
+    if(a[0]) {
+        counter += area(a[1])
+//        println "counter=${counter}"
+    }
 }
-
-println count(areas, true)
+println counter
