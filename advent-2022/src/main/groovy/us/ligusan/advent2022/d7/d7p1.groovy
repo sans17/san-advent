424ae756c3365e867558bdf74b5c05dcb82e225d
AlexP <san@ligusan.us>
2022-12-07 11:50:02 -0500

    day 7

---
+++ advent-2022/src/main/groovy/us/ligusan/advent2022/d7/d7p1.groovy
@@ -0,0 +1,72 @@
+package us.ligusan.advent2022.d7
+
+List<String> list = new File(getClass().getResource('input.txt').toURI()).collect {it}
+
+lsFlag = false
+processed = [] as Set
+sizeMap = [:]
+
+cd = '/'
+list.each { line ->
+    println "line=$line"
+    
+    if (line[0] == '$') {
+        if (line == '$ ls') {
+            lsFlag = true
+        } else {
+            if (lsFlag) {
+                lsFlag = false
+                processed += cd
+                
+                println "processed=$processed"
+            }
+            
+            split = (line =~ /\$ cd (.+)/)[0]
+//            println "split=$split"
+            
+            switch (split[1]) {
+                case '/':
+                    cd = '/'
+                    break
+                    
+                case '..':
+                    cd = cd.replaceFirst('[^/]+/$', '');
+                    break
+                    
+                default:
+                    cd = cd + split[1] + '/'
+            }
+            println "cd=$cd"
+
+            sizeMap.putIfAbsent(cd, 0)
+
+            println "sizeMap=$sizeMap"
+        }
+    } else if (!processed.contains(cd)) {
+        split = (line =~ /(dir|\d+) (.+)/)[0]
+//        println "split=$split"
+        
+        if ('dir' != split[1]) {
+            sizeMap.each { entry ->
+                if (cd.startsWith(entry.key)) {
+                    entry.value = entry.value + new BigInteger(split[1])
+                    
+                    println "entry=$entry"
+                }
+            }
+        }
+    }
+}
+
+println "sizeMap=$sizeMap"
+
+total = 0
+sizeMap.values().each {
+    println "it=$it" 
+    if (it < 100_000) {
+        total += it
+        println "total=$total"
+    }
+}
+
+println total
