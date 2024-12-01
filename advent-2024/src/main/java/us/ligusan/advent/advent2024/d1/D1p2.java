83cd6be7fa6faf442b41b6fd71a2ae6171ed153c
AlexP <san@ligusan.us>
2024-12-01 15:46:35 -0500

    maps correction

--- advent-2024/src/main/java/us/ligusan/advent/advent2024/d1/D1p2.java
+++ advent-2024/src/main/java/us/ligusan/advent/advent2024/d1/D1p2.java
@@ -1,14 +1,12 @@
 package us.ligusan.advent.advent2024.d1;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class D1p2 {
     public static void main(final String[] args) {
-        final var left = new ArrayList<Integer>();
-
+        final var leftCount = new HashMap<Integer, Integer>();
         final var rightCount = new HashMap<Integer, Integer>();
 
         try (var scanner = new Scanner(D1p2.class.getResourceAsStream("input.txt"))) {
@@ -16,13 +14,16 @@ public class D1p2 {
                 System.out.println(s);
 
                 Pattern.compile("(\\d+)\\s+(\\d+)").matcher(s).results().forEach(m -> {
-                    left.add(Integer.parseInt(m.group(1)));
+                    leftCount.compute(Integer.parseInt(m.group(1)), (k, v) -> v == null ? 1 : v + 1);
                     rightCount.compute(Integer.parseInt(m.group(2)), (k, v) -> v == null ? 1 : v + 1);
                 });
             });
         }
-        System.out.format("left=%s%nrightCount=%s%n", left, rightCount);
+        System.out.format("leftCount=%s%nrightCount=%s%n", leftCount, rightCount);
 
-        left.stream().mapToInt(i -> i * rightCount.getOrDefault(i, 0)).reduce(Integer::sum).ifPresent(System.out::println);
+        leftCount.entrySet().stream().mapToInt(e -> {
+            var key = e.getKey();
+            return key * rightCount.getOrDefault(key, 0) * e.getValue();
+        }).reduce(Integer::sum).ifPresent(System.out::println);
     }
 }
